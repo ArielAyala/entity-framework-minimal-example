0df076641c59fd7ea1d62f0265acd8ad80fbc21b
Ariel Ayala <arielayalamail@gmail.com>
2022-06-19 14:39:59 -0400

    Setup dbcontext

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using entity_framework_minimal_example;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<EfMainContext>(p=> p.UseInMemoryDatabase("EFMinimalExampleDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] EfMainContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Database created in memory " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
